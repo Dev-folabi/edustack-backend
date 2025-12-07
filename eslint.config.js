3d0106304c383d9d6b1ee87b8a64aced4594d970
Dev-folabi <yusufafolabi95@gmail.com>
2025-12-07 17:44:21 +0100

    chore: Remove explicit ES module type, add ESLint ignore paths, and refine linting rules.

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
-import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import globals from "globals";
 
 /** @type {import("eslint").FlatConfig[]} */
 export default [
+  {
+    ignores: ["dist/**", "node_modules/**"],
+  },
   // Enable ESLint for TypeScript and JavaScript files
   {
     files: ["**/*.{js,mjs,cjs,ts}"],
@@ -12,28 +14,26 @@ export default [
       parserOptions: {
         ecmaVersion: "latest",
         sourceType: "module",
-        project: "./tsconfig.json", // Points to your TypeScript config
       },
-      globals: globals.node, // Enable Node.js globals (e.g., process, __dirname)
+      globals: globals.node,
     },
     plugins: {
       "@typescript-eslint": tseslint.plugin,
     },
     rules: {
-      ...js.configs.recommended.rules,
       ...tseslint.configs.recommended.rules,
       // Custom rules
       "no-unused-vars": "off",
       "no-console": "warn",
-      "semi": ["error", "always"],
-      "strict": ["error", "global"],
+      semi: ["error", "always"],
+      strict: ["error", "global"],
       "no-undef": "off",
       "no-mixed-spaces-and-tabs": "error",
       "prefer-const": "warn",
       "no-trailing-spaces": "warn",
       "no-use-before-define": "error",
       "no-unused-expressions": "error",
-      "eqeqeq": "error",
+      eqeqeq: "error",
       "no-else-return": "error",
       "no-empty-function": "error",
       "block-scoped-var": "error",
